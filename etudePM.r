a4835ea96ed9883f3acfcc9ee13e940e74a9bab6
Utilisateur <user@debian.arcahe.ovh>
2020-04-12 14:28:36 +0200

    ajout nouveau rapport

--- etudePM.r
+++ etudePM.r
@@ -10,14 +10,14 @@ questionnaire<-read.csv("questionnaire_participants.csv",header=TRUE,sep=",")
 
 #~ requête qui montre la moyenne des PM en fonction des differente activity
 
-sql0 = "SELECT activity, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' , ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)'
+sql0 = "SELECT activity, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' , ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)'
 
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		GROUP BY activity
 		EXCEPT 
 
-		SELECT activity, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' , ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)'
+		SELECT activity, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' , ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)'
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		AND activity = 'NULL';"
@@ -130,20 +130,59 @@ sql7 = "SELECT activity, event , avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'M
 
 Voiture <-sqldf(sql7)
 
-sql8 = "SELECT df.participant_virtual_id, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,min(\"PM2.5\") as 'MIN_PM2.5' ,max(\"PM2.5\")  as 'MAX_PM2.5', ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',min(\"PM10\") as 'MIN_PM10' ,max(\"PM10\")  as 'MAX_PM10',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',min(\"PM1.0\") as 'MIN_PM1.0' ,max(\"PM1.0\")  as 'MAX_PM1.0',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',min(cast(NO2 AS INTEGER)) as 'MIN_NO2' ,max(cast(NO2 AS INTEGER)) as 'MAX_NO2',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)',min(cast(BC AS INTEGER)) as 'MIN_BC' ,max(cast(BC AS INTEGER)) as 'MAX_BC'
+#~ requête qui montre la moyenne des PM en fonction de chaque participant
+sql8 = "SELECT df.participant_virtual_id, avg(\"PM2.5\") AS 'MOY_PM2.5',avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0',avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		GROUP BY df.participant_virtual_id
 		EXCEPT 
-		SELECT df.participant_virtual_id, ROUND(avg(\"PM2.5\"),2) AS 'MOY_PM2.5',ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,min(\"PM2.5\") as 'MIN_PM2.5' ,max(\"PM2.5\")  as 'MAX_PM2.5', ROUND(avg(PM10),2) as 'MOY_PM10' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',min(\"PM10\") as 'MIN_PM10' ,max(\"PM10\")  as 'MAX_PM10',ROUND(avg(\"PM1.0\"),2) as 'MOY_PM1.0',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',min(\"PM1.0\") as 'MIN_PM1.0' ,max(\"PM1.0\")  as 'MAX_PM1.0',ROUND(avg(NO2),2) as 'MOY_NO2',ROUND(stdev(NO2),2) as 'Ecart(NO2)',min(cast(NO2 AS INTEGER)) as 'MIN_NO2' ,max(cast(NO2 AS INTEGER))  as 'MAX_NO2',ROUND(avg(BC),2) as 'MOY_BC',ROUND(stdev(BC),2) as 'Ecart(BC)',min(cast(BC AS INTEGER)) as 'MIN_BC' ,max(cast(BC AS INTEGER))  as 'MAX_BC'
+		SELECT df.participant_virtual_id, avg(\"PM2.5\") AS 'MOY_PM2.5',avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0',avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		AND activity = 'NULL';"
 		
-participant<-sqldf(sql8)
+moy_participant<-sqldf(sql8)
 
+#~ requête qui montre l'écart à la moyenne de chaque polluant
+sql9 = "SELECT df.participant_virtual_id, ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(stdev(BC),2) as 'Ecart(BC)'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		GROUP BY df.participant_virtual_id
+		EXCEPT 
+		SELECT df.participant_virtual_id, ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(stdev(BC),2) as 'Ecart(BC)'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND activity = 'NULL';"
+		
+ecart_moy<-sqldf(sql9)
 
+#~ requête qui montre la moyenne des PM du participant 9999932 en fonction des differente activity
+sql10 = "SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999932'
+		GROUP BY activity
+		EXCEPT 
+		SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999932'
+		AND activity = 'NULL';"
 
+p9999932 <-sqldf(sql10)
 
+#~ requête qui montre la moyenne des BC des participant qui sont exposer en fonction des differente activity
+sql11 = "SELECT activity, avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		GROUP BY activity
+		EXCEPT 
+		SELECT activity, avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND activity = 'NULL';
+		HAVING AVG(BC)= '0' "
+
+BC <-sqldf(sql11)
 
 
