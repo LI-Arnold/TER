49f3733bb00be6e16829068126b324b516f757d1
Utilisateur <user@debian.arcahe.ovh>
2020-04-16 18:00:05 +0200

    update rapport

--- etudePM.r
+++ etudePM.r
@@ -131,7 +131,7 @@ sql7 = "SELECT activity, event , avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'M
 Voiture <-sqldf(sql7)
 
 #~ requête qui montre la moyenne des PM en fonction de chaque participant
-sql8 = "SELECT df.participant_virtual_id, avg(\"PM2.5\") AS 'MOY_PM2.5',avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0',avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+sql8 = "SELECT df.participant_virtual_id AS Participant, avg(\"PM2.5\") AS 'MOY_PM2.5',avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0',avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		GROUP BY df.participant_virtual_id
@@ -144,7 +144,7 @@ sql8 = "SELECT df.participant_virtual_id, avg(\"PM2.5\") AS 'MOY_PM2.5',avg(PM10
 moy_participant<-sqldf(sql8)
 
 #~ requête qui montre l'écart à la moyenne de chaque polluant
-sql9 = "SELECT df.participant_virtual_id, ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(stdev(BC),2) as 'Ecart(BC)'
+sql9 = "SELECT df.participant_virtual_id AS Participant, ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM2.5)' ,ROUND(stdev(\"PM10\"),2) as 'Ecart(PM10)',ROUND(stdev(\"PM1.0\"),2) as 'Ecart(PM1.0)',ROUND(stdev(NO2),2) as 'Ecart(NO2)',ROUND(stdev(BC),2) as 'Ecart(BC)'
 		FROM df, questionnaire
 		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
 		GROUP BY df.participant_virtual_id
@@ -155,6 +155,21 @@ sql9 = "SELECT df.participant_virtual_id, ROUND(stdev(\"PM2.5\"),2) as 'Ecart(PM
 		AND activity = 'NULL';"
 		
 ecart_moy<-sqldf(sql9)
+
+#~ requête qui montre la moyenne des PM du participant 9999932 en fonction des differente activity
+sql10 = "SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999932'
+		GROUP BY activity
+		EXCEPT 
+		SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999932'
+		AND activity = 'NULL';"
+
+p9999932 <-sqldf(sql10)
 
 #~ requête qui montre la moyenne des PM du participant 9999932 en fonction des differente activity
 sql10 = "SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
@@ -185,4 +200,90 @@ sql11 = "SELECT activity, avg(BC) as 'MOY_BC'
 
 BC <-sqldf(sql11)
 
+#~ requête qui montre la moyenne des PM du participant 9999944 en fonction des differente activity
+sql12 = "SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999944'
+		GROUP BY activity
+		EXCEPT 
+		SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999944'
+		AND activity = 'NULL';"
 
+p9999944 <-sqldf(sql12)
+
+#~ requête qui montre la moyenne des PM du participant 9999946 en fonction des differente activity
+sql13 = "SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999946'
+		GROUP BY activity
+		EXCEPT 
+		SELECT activity, avg(\"PM2.5\") AS 'MOY_PM2.5' , avg(PM10) as 'MOY_PM10' ,avg(\"PM1.0\") as 'MOY_PM1.0', avg(NO2) as 'MOY_NO2',avg(BC) as 'MOY_BC'
+		FROM df, questionnaire
+		WHERE df.participant_virtual_id = questionnaire.participant_virtual_id
+		AND df.participant_virtual_id = '9999946'
+		AND activity = 'NULL';"
+
+p9999946 <-sqldf(sql13)
+
+sql14 ="SELECT  df.participant_virtual_id AS Participant,
+        CASE 
+        WHEN \"q_17.1\"='jamais'  THEN 'Non sportif' 
+        ELSE 'Sportif'   
+		END	as  Categorie
+		FROM questionnaire,df 
+		WHERE questionnaire.participant_virtual_id = df.participant_virtual_id AND \"q_17.1\" IS NOT NULL
+		GROUP BY df.participant_virtual_id"
+		
+sportif <- sqldf(sql14)
+
+sql16 ="SELECT  df.participant_virtual_id AS Participant,
+		CASE
+		WHEN  q_48_1='Oui' or q_48_2='Oui' or q_48_3='Oui' or q_48_4='Oui' or q_48_5='Oui' or q_48_6='Oui' or q_48_7='Oui' or q_48_8='Oui' or q_48_9='Oui' or q_49_1='Oui' or q_49_2='Oui' or q_49_3='Oui' or q_49_4='Oui' or q_49_5='Oui' or q_49_6='Oui' or q_49_7='Oui' or q_49_8='Oui' or q_49_9='Oui' 
+		THEN 'Sensible a la pollution' 
+		ELSE 'Non Sensible a la pollution'	
+		END	as  Categorie
+		FROM questionnaire,df 
+		WHERE questionnaire.participant_virtual_id = df.participant_virtual_id 
+		GROUP BY df.participant_virtual_id ;"
+		
+sensible <- sqldf(sql16)
+
+sql17 ="SELECT  df.participant_virtual_id AS Participant,
+		CASE	
+		WHEN  q_59_1<1995 and q_59_1>1985
+		THEN 'Age entre 20 et 35'
+		WHEN  q_59_1<1985 and q_59_1>1970
+		THEN 'Age entre 35 et 50'
+		WHEN  q_59_1<1970
+		THEN 'Age plus de 50'	
+		END	as  Categorie
+		FROM questionnaire,df 
+		WHERE questionnaire.participant_virtual_id = df.participant_virtual_id
+		AND q_59_1 not null
+		GROUP BY df.participant_virtual_id; "
+		
+age<- sqldf(sql17)
+
+sql18 ="SELECT   df.participant_virtual_id AS Participant,
+		CASE	
+		WHEN  q_58='Un homme'
+		THEN 'Un homme'
+		WHEN  q_58='Une femme'
+		THEN 'Une femme'	
+		END	as  Categorie
+		FROM questionnaire,df 
+		WHERE questionnaire.participant_virtual_id = df.participant_virtual_id 
+		AND q_58 IS NOT NULL
+		GROUP BY df.participant_virtual_id; "
+
+sexe <- sqldf(sql18)		
+		
+		
+		
+		
+		
